305e2dc0481bac167f55786f8a506c7e90a92a10
Rinitha Rajan <rinitzrajan@gmail.com>
2025-02-28 17:03:01 +0530

    checkbox try

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,17 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.header('Multiselect')
+st.header('Checkbox')
 
-options = st.multiselect(
-    'What are your fave colours?',
-    ['Green', 'Yellow', 'Red', 'Blue'],
-    ['Yellow', 'Red']
-)
+st.write('What would you like to eat?')
 
-st.write('You selected: ', options)
+food1 = st.checkbox('Ice Cream')
+food2 = st.checkbox('Coffee')
+food3 = st.checkbox('Cola')
+
+if food1:
+    st.write('You chose ice cream')
+elif food2:
+    st.write('You chose Coffee')
+elif food3:
+    st.write('You chose Cola')
